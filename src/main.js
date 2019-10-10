8103228674e4ccd29a99966060643113744ebdc7
__单调 <wangli569638598@163.com>
2019-10-10 14:08:10 +0800

    'firstCommit'

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false
+
+
+Axios.defaults.baseURL = "";
+Vue.prototype.ajax = Axios;
+
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
